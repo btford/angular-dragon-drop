1c986f9d0eae3251676f4c53d1e72582cd127897
David Simon <dsimon@americancouncils.org>
2013-08-14 15:35:05 -0400

    Added reappend-on-drop test, cleaned up test code

--- test/spec.js
+++ test/spec.js
@@ -9,52 +9,71 @@ describe('angular-dragon-drop', function() {
   beforeEach(function() {
     if (!server) {
       server = child_process.spawn('./test/webserver.js');
-      server.unref();
+      server.unref(); // Server will stop when the protractor process ends
     }
+
+    ptor.get('http://localhost:8006/test/page.html');
   });
 
   var ptor = protractor.getInstance();
 
-  var listElements = function (id, f) {
-    ptor.findElement(protractor.By.id(id))
-    .findElements(protractor.By.tagName("li"))
-    .then(f);
+  var findListElements = function (id, f) {
+    return ptor.findElement(protractor.By.id(id))
+    .findElements(protractor.By.tagName("li"));
   };
 
-  it('initializes the lists correctly', function() {
-    ptor.get('http://localhost:8006/test/simple.html');
-
-    listElements("thingsList", function (elements) {
-      expect(elements.length).toEqual(3);
-      for (var i = 0; i < elements.length; ++i) {
-        expect(elements[i].getText()).toEqual(String(i));
-      }
-    });
-
-    listElements("otherThingsList", function (elements) {
-      expect(elements.length).toEqual(0);
-    });
-  });
-
-  it('moves items between lists when dragged', function() {
-    ptor.get('http://localhost:8006/test/simple.html');
-
-    listElements("otherThingsList", function (elements) {
-      expect(elements.length).toEqual(0);
-    });
-
-    ptor.findElement(protractor.By.id("item0"))
+  var dragAndDropItem = function(srcId, dstId) {
+    ptor.findElement(protractor.By.id(srcId))
     .then(function(srcElem) {
-      ptor.findElement(protractor.By.id("otherThingsList"))
+      ptor.findElement(protractor.By.id(dstId))
       .then(function(dest) {
         new webdriver.ActionSequence(ptor.driver)
         .dragAndDrop(srcElem, dest)
         .perform();
       });
     });
+  };
 
-    listElements("otherThingsList", function (elements) {
+  it('initializes the lists correctly', function() {
+    findListElements("thingsList").then(function(elements) {
+      expect(elements.length).toEqual(3);
+      expect(elements[0].getText()).toEqual("0");
+      expect(elements[1].getText()).toEqual("1");
+      expect(elements[2].getText()).toEqual("2");
+    });
+
+    findListElements("otherThingsList").then(function(elements) {
+      expect(elements.length).toEqual(0);
+    });
+  });
+
+  it('moves items between lists when dragged', function() {
+    dragAndDropItem("item1", "otherThingsList");
+
+    findListElements("otherThingsList").then(function(elements) {
       expect(elements.length).toEqual(1);
+      expect(elements[0].getText()).toEqual("1");
+    });
+
+    findListElements("thingsList").then(function(elements) {
+      expect(elements.length).toEqual(2);
+      expect(elements[0].getText()).toEqual("0");
+      expect(elements[1].getText()).toEqual("2");
+    });
+  });
+
+  it('reappends items to their own list when dragged to nowhere', function() {
+    dragAndDropItem("item1", "boring");
+
+    findListElements("thingsList").then(function(elements) {
+      expect(elements.length).toEqual(3);
+      expect(elements[0].getText()).toEqual("0");
+      expect(elements[1].getText()).toEqual("2");
+      expect(elements[2].getText()).toEqual("1"); // Item1 moved to the end
+    });
+
+    findListElements("otherThingsList").then(function(elements) {
+      expect(elements.length).toEqual(0);
     });
   });
 });
